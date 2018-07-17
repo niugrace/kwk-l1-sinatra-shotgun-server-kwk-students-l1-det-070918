5fe0f6e2c30aa531497f7ceb0e45009bea3c30f2
niugrace <grace.niu.01@gmail.com>
2018-07-17 19:10:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Started my server using Shotgun!"
-  end
+get '/hello/:id' do 
+  @name = params[:id]
+  "Hello #{@name}!"
+end 
+
+
 
 end
